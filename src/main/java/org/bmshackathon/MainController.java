127242d1710a00bad4c16a10163c580ccd68999f
Daniel Wojda <daniel.wojda@bms.com.pl>
2015-09-17 15:37:12 +0200

    Walking skeleton.

---
+++ src/main/java/org/bmshackathon/MainController.java
@@ -0,0 +1,17 @@
+package org.bmshackathon;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.Map;
+
+@Controller
+class MainController {
+
+    @RequestMapping("/")
+    public String index(Map<String, Object> model) {
+        model.put("time", new Date());
+        return "index";
+    }
+}
