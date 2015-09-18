58edf12c1d57ebdf05e692bf97987c6c92a86fd1
Daniel Wojda <wojdadan@gmail.com>
2015-09-18 21:38:05 +0200

    Add Video details page.

--- src/main/java/org/bmshackathon/MainController.java
+++ src/main/java/org/bmshackathon/MainController.java
@@ -2,9 +2,13 @@ package org.bmshackathon;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @Controller
 class MainController {
@@ -15,9 +19,19 @@ class MainController {
         this.repository = repository;
     }
 
-    @RequestMapping("/")
+    @RequestMapping(value="/", method = GET)
     public String index(Map<String, Object> model) {
         model.put("videos", repository.findAll());
         return "index";
+    }
+
+    @RequestMapping(value = "/video/{id}/details", method = GET)
+    public String videoDetail(@PathVariable Long id,  Map<String, Object> model) {
+        VideoTeaser maybeVideoTeaser = repository.findOne(id);
+        if(maybeVideoTeaser == null) {
+            throw new IllegalArgumentException("Video with id="+id+" does not exist");
+        }
+        model.put("video", maybeVideoTeaser);
+        return "details";
     }
 }
