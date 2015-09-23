1132e8ef6fdb5fd54814a62a401af6ec5ec5dfa1
Daniel Wojda <wojdadan@gmail.com>
2015-09-23 16:57:29 +0200

    Remove not used throw clause.

--- src/main/java/org/bmshackathon/VideoRepository.java
+++ src/main/java/org/bmshackathon/VideoRepository.java
@@ -4,11 +4,10 @@ package org.bmshackathon;
 import org.bmshackathon.video.Video;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-import javax.management.ServiceNotFoundException;
 import java.util.List;
 
 @RepositoryRestResource
 public interface VideoRepository {
-    List<Video> findAll() throws ServiceNotFoundException;
+    List<Video> findAll();
     List<Video> findByUuid(Long uuid);
 }
