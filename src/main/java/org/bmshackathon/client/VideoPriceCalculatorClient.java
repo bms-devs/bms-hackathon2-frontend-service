d3ae29a8d219a38ff197657e8815c3e5cdfe99cb
Łukasz Kamiński <lkamins7@gmail.com>
2015-09-25 23:19:58 +0200

    Added a circuit breaker.

--- src/main/java/org/bmshackathon/client/VideoPriceCalculatorClient.java
+++ src/main/java/org/bmshackathon/client/VideoPriceCalculatorClient.java
@@ -1,14 +1,23 @@
 package org.bmshackathon.client;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.stereotype.Component;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.math.BigDecimal;
 
+@EnableCircuitBreaker
 @Component
 public class VideoPriceCalculatorClient {
 
-    //TODO
+    @HystrixCommand(fallbackMethod = "defaultPrice")
     public BigDecimal calculateFor(Long id) {
-        return BigDecimal.valueOf(999999999.01);
+        throw new NotImplementedException();
+    }
+
+    public BigDecimal defaultPrice(Long id) {
+        // It's free!
+        return BigDecimal.valueOf(0.0);
     }
 }
