8d8f2af169f2e00e2963720c27cad20984875618
p-bms <p-bms@users.noreply.github.com>
2015-09-26 14:03:38 +0200

    Added Spring Cloud Sleuth

--- src/main/java/org/bmshackathon/FacadeServiceApplication.java
+++ src/main/java/org/bmshackathon/FacadeServiceApplication.java
@@ -5,12 +5,20 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.sleuth.Sampler;
+import org.springframework.cloud.sleuth.sampler.AlwaysSampler;
+import org.springframework.context.annotation.Bean;
 
 @EnableEurekaClient
 @EnableFeignClients
 @EnableHystrix
 @SpringBootApplication
 public class FacadeServiceApplication {
+
+    @Bean
+    public Sampler<?> defaultSampler() {
+        return new AlwaysSampler();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(FacadeServiceApplication.class, args);
